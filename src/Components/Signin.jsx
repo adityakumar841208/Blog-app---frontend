55886b324c0fdc7456694eb93dafc0fe141f5609
adityakumar841208 <165656490+adityakumar841208@users.noreply.github.com>
2024-08-23 08:40:00 +0530

    Update Signin.jsx as SignUp

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const Signin = () => {
+const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate()
@@ -69,4 +69,4 @@ const Signin = () => {
   );
 };
 
-export default Signin;
+export default SignUp;
